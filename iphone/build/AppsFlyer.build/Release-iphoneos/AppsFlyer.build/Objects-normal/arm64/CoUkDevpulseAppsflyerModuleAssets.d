5244267108c7d69ea752281053e9605bcf84cc12
Klaudia Nemeth <knemeth@vidatec.com>
2021-01-18 17:13:05 +0100

    delete build

--- iphone/build/AppsFlyer.build/Release-iphoneos/AppsFlyer.build/Objects-normal/arm64/CoUkDevpulseAppsflyerModuleAssets.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/jayy/Programming/TiModules/AppsFlyer/iphone/Classes/CoUkDevpulseAppsflyerModuleAssets.m \
-  /Users/jayy/Programming/TiModules/AppsFlyer/iphone/Classes/CoUkDevpulseAppsflyerModuleAssets.h
